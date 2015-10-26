2228c12bec661dcd82bb4d25d20f554a980cf124
785693057 <785693057@QQ.COM>
2015-10-26 13:09:11 +0800

    = =

--- server/src/main/dataservice/billdataservice/BilldataService.java
+++ server/src/main/dataservice/billdataservice/BilldataService.java
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-=======
 package main.dataservice.billdataservice;
->>>>>>> 094f8eb0b5ad53da40bdd1ad2746d6c9c138237c
 
 /**
  *Created by ZHR
  *2015年10月26日
  */
 
-public interface BillDataService {
+public interface BilldataService {
 
 }
--- server/src/main/dataservice/storedataservice/StoreDataService.java
+++ server/src/main/dataservice/storedataservice/StoreDataService.java
@@ -1,3 +1,5 @@
+package main.dataservice.storedataservice;
+
 
 /**
  *Created by ZHR
