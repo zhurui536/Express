a3442936998c70966e85b48da3422056cc42b096
785693057 <785693057@QQ.COM>
2015-10-29 11:25:51 +0800

    写了一点关于库存的对象

--- client/src/main/po/StorePO.java
+++ client/src/main/po/StorePO.java
@@ -1,6 +1,7 @@
 package main.po;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * 库存PO
@@ -9,4 +10,57 @@ import java.io.Serializable;
  */
 
 public class StorePO implements Serializable {
+	//将所有库存位置整合到一起成为一个仓库对象
+	private ArrayList<StorePlacePO> store;
+	//区数
+	private int areas;
+	//排数
+	private int rows;
+	//架数
+	private int shelfs;
+	//位数
+	private int places;
+	
+	/*构造一个固定尺寸的仓库对象*/
+	public StorePO(int areas, int rows, int shelfs, int places){
+		this.areas = areas;
+		this.rows = rows;
+		this.shelfs = shelfs;
+		this.places = places;
+		
+		for(int area=1;area<=areas;area++){
+			for(int row=1;row<=rows;row++){
+				for(int shelf=1;shelf<=shelfs;shelf++){
+					for(int place=1;place<=places;place++){
+						StorePlacePO storeplace = new StorePlacePO(area, row, shelf, place);
+						
+						store.add(storeplace);
+					}
+				}
+			}
+		}
+		
+	}
+	
+	/*获得在某一位置上的库存信息*/
+	public StorePlacePO getStorePlace(int area, int row, int shelf, int place){
+		int index = 0;
+		index = areas*(area-1) + rows*(row-1) + shelfs*(shelf-1) + place;
+		
+		return store.get(index);
+	}
+	
+	/*改变某一位置上的库存状态*/
+	public boolean setStorePlace(StorePlacePO storeplace){
+		int area = storeplace.getArea();
+		int row = storeplace.getRow();
+		int shelf = storeplace.getShelf();
+		int place = storeplace.getPlace();
+		
+		int index = 0;
+		index = areas*(area-1) + rows*(row-1) + shelfs*(shelf-1) + place;
+		
+		store.set(index, storeplace);
+		return true;
+	}
 }
--- client/src/main/po/StorePlacePO.java
+++ client/src/main/po/StorePlacePO.java
@@ -5,24 +5,37 @@ package main.po;
  * 2015/10/26
  */
 public class StorePlacePO {
+	//区号
 	private int area;
+	//排号
 	private int row;
+	//架号
 	private int shelf;
+	//位号
 	private int place;
+	//该位置存放的货物
+	private GoodsPO goods;
 	
 	public StorePlacePO(int area, int row, int shelf, int place){
 		this.area = area;
 		this.row = row;
 		this.shelf = shelf;
 		this.place = place;
+		this.goods = null;
 	}
 
 	public boolean ifEmpty(){
 		return true;
 	}
 	
-	public GoodsPO getGood(){
-		return null;
+	public GoodsPO getGoods(){
+		return this.goods;
+	}
+	
+	public boolean setGoods(GoodsPO goods){
+		this.goods = goods;
+		
+		return true;
 	}
 	
 	public int getArea(){
