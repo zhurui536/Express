70290a181ad094774668490ce92c64d6c20c8825
away <2464226202@qq.com>
2015-11-25 22:05:59 +0800

    class

---
+++ server/main/data/test.java
@@ -0,0 +1,9 @@
+package main.data;
+
+/**
+ * Created by Away
+ * 2015/11/25
+ */
+
+public class test {
+}
---
+++ server/main/dataservice/test.java
@@ -0,0 +1,9 @@
+package main.dataservice;
+
+/**
+ * Created by Away
+ * 2015/11/25
+ */
+
+public class test {
+}
