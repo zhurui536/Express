a0359904068e049a8ba27cd5befab2392b4341e1
785693057 <785693057@QQ.COM>
2015-12-07 13:25:45 +0800

    11

--- common/dataservice/billdataservice/BilldataService.java
+++ common/dataservice/billdataservice/BilldataService.java
@@ -3,19 +3,18 @@ package dataservice.billdataservice;
 import java.util.ArrayList;
 
 import main.bussinesslogic.util.ResultMessage;
-import po.BillPO;
 
 /**
  *Created by ZHR
  *2015年10月26日
  */
 
-public interface BilldataService {
-	public BillPO find(String id);
+public interface BillDataService {
+	public ResultMessage find(String id);
 	
 	public ResultMessage approve(String id);
 	
-	public ArrayList<BillPO> getBills();
+	public ResultMessage getBills();
 	
 	public ResultMessage approves(ArrayList<String> ids);
 	
