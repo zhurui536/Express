5768cf2666cf9de3acbdef26c6ccb37436e21dc4
zhuding <zhuding@zhuding-ThinkPad-Edge-E440>
2015-12-08 17:08:29 +0800

    好像有个报错的=。=

--- client/test/tester/logisticstester/ReceivingTester.java
+++ client/test/tester/logisticstester/ReceivingTester.java
@@ -1,11 +1,8 @@
 package test.tester.logisticstester;
 
 import static org.junit.Assert.*;
-import main.bussinesslogic.logisticsbl.LogisticsBLController;
-import main.bussinesslogic.util.BillType;
-import main.bussinesslogic.util.ResultMessage;
-import main.bussinesslogicservice.logisticsblservice.LogisticsBLService;
-import main.vo.BillVO;
+
+
 
 import org.junit.Test;
 
@@ -13,7 +10,7 @@ public class ReceivingTester {
     
     @Test
     public void test() {
-        LogisticsBLService logisticsBLService = new LogisticsBLController();
+//        LogisticsBLService logisticsBLService = new LogisticsBLController();
 //        ResultMessage message = logisticsBLService.addMessage(new BillVO("null", BillType.SEND));
         
         assertEquals("success",null);
