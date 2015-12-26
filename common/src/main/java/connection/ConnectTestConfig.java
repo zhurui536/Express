5002a884f9757681de127460ee033adc1951b275
away <2464226202@qq.com>
2015-12-26 16:04:55 +0800

    aa

---
+++ common/src/main/java/connection/ConnectTestConfig.java
@@ -0,0 +1,17 @@
+package connection;
+
+public class ConnectTestConfig {
+	
+	private static String sendMsg = "seng";
+	
+	private static String successMsg = "get";
+
+	public static String getSendMsg() {
+		return sendMsg;
+	}
+
+	public static String getSuccessMsg() {
+		return successMsg;
+	}
+	
+}
