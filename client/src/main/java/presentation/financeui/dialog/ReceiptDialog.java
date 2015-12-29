ecf0c101fe657231daab9906636deb83379ff2c1
away <2464226202@qq.com>
2015-12-30 01:30:18 +0800

    改了Location

--- client/src/main/java/presentation/financeui/dialog/ReceiptDialog.java
+++ client/src/main/java/presentation/financeui/dialog/ReceiptDialog.java
@@ -3,6 +3,7 @@ package presentation.financeui.dialog;
 import presentation.financeui.FinanceFrame;
 import presentation.mainui.InputFrame;
 import presentation.mainui.component.ToolButton;
+import util.DateChooser;
 import util.Time;
 
 import javax.swing.*;
@@ -18,9 +19,7 @@ import java.awt.event.ActionListener;
 @SuppressWarnings("serial")
 public class ReceiptDialog extends InputFrame {
 
-    private JComboBox<String> year;
-    private JComboBox<String> month;
-    private JComboBox<String> day;
+	private DateChooser dateChooser;
     private JTextField id;
     private Time time;
     private String ID;
@@ -35,59 +34,38 @@ public class ReceiptDialog extends InputFrame {
         this.setBounds(ui.getX() + 300, ui.getY() + 200, 400, 200);
         this.setBackgroundSize(400, 200);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        this.setModal(true);
 
         JPanel panel = new JPanel();
 
         JButton ok = new ToolButton(150, 120,"确定");
-        ok.setSize( 80, 30);
+        ok.setSize(80, 30);
         ok.addActionListener(new okListener());
 
         JLabel time = new JLabel("时间");
         time.setBounds(50, 20, 80, 50);
 
-        year = new JComboBox<>();
-        for (int i = 2000; i <= 2020; i++) {
-            year.addItem(i + "");
-        }
-        year.setBounds(150, 30, 100, 30);
-
-        month = new JComboBox<>();
-        for (int i = 1; i <= 12; i++) {
-            month.addItem(i + "");
-        }
-        month.setBounds(260, 30, 50, 30);
-
-        day = new JComboBox<>();
-        for (int i = 1; i <= 31; i++) {
-            day.addItem(i + "");
-        }
-        day.setBounds(320, 30, 50, 30);
+        dateChooser = new DateChooser(this);
+        dateChooser.setBounds(105, 30, 200, 30);
 
         JLabel id = new JLabel("营业厅id");
         id.setBounds(50, 70, 80, 50);
 
         this.id = new JTextField();
-        this.id.setBounds(150, 80, 150, 30);
+        this.id.setBounds(120, 80, 130, 30);
 
         panel.setLayout(null);
         panel.add(ok);
-        panel.add(year);
-        panel.add(month);
         panel.add(id);
-        panel.add(day);
         panel.add(this.id);
         panel.add(time);
+        panel.add(dateChooser);
         this.setContentPane(panel);
     }
 
     private class okListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-        	String y = (String) year.getSelectedItem();
-            String m = (String) month.getSelectedItem();
-            String d = (String) day.getSelectedItem();
-            time = new Time(y + "-" + m + "-" + d);
+            time = new Time(dateChooser.getDateField().getText());
             ID = id.getText();
             close();
         }
